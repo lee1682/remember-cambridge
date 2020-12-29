7d1f88f0aa070231bb5efd43e7ec836ba029fd9f
leeoneill102030 <leeoneill102030@gmail.com>
2020-12-29 17:08:13 +0000

    set up

--- wp-content/themes/remember-cambridge/front-page.php
+++ wp-content/themes/remember-cambridge/front-page.php
@@ -1,5 +1,17 @@
 <?php /* Template Name: front-page */ get_header(); ?>
 
+<?php get_template_part("sections/intro-text" );?>
 
+<section class="text-and-image"></section>
+
+<section class="full-width-cta-banner"></section>
+
+<section class="featured-post"></section>
+
+<section class="3-col-highlights"></section>
+
+<section class="3-col-post-thumbs"></section>
+
+<section class="contact-cta"></section>
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/remember-cambridge/sections/intro-text.php
@@ -0,0 +1,5 @@
+<section class="intro-text">
+    <h2> Category Intro Text</h2>
+
+    <p> Topping fruitcake gummi bears fruitcake caramels marzipan. Donut donut sugar plum topping gummi bears. Marzipan soufflé sweet roll chocolate bar muffin chupa chups lemon drops cheesecake. Gummi bears donut croissant powder. Sesame snaps jelly beans caramels gummies macaroon liquorice. Cake biscuit powder oat cake danish candy canes. Jelly beans muffin marshmallow chupa chups gingerbread muffin cake jujubes jelly. Halvah jelly-o bear claw oat cake. Cookie pastry carrot cake biscuit gummies. Danish cupcake marzipan.</p>
+</section>
