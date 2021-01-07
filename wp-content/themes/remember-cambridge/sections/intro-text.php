154d439e0ee51b7fc1f8c9fbd5fd6b3f81c20a85
leeoneill102030 <leeoneill102030@gmail.com>
2021-01-07 20:07:33 +0000

    intro text

--- wp-content/themes/remember-cambridge/sections/intro-text.php
+++ wp-content/themes/remember-cambridge/sections/intro-text.php
@@ -1,5 +1,8 @@
-<section class="intro-text container-large flex-column  ">
-    <h2> Category Intro Text</h2>
+<section class="intro-text container flex-column  ">
+    <div class="col-12">
+      <h2> Category Intro Text</h2>
 
-    <p> Topping fruitcake gummi bears fruitcake caramels marzipan. Donut donut sugar plum topping gummi bears. Marzipan soufflé sweet roll chocolate bar muffin chupa chups lemon drops cheesecake. Gummi bears donut croissant powder. Sesame snaps jelly beans caramels gummies macaroon liquorice. Cake biscuit powder oat cake danish candy canes. Jelly beans muffin marshmallow chupa chups gingerbread muffin cake jujubes jelly. Halvah jelly-o bear claw oat cake. Cookie pastry carrot cake biscuit gummies. Danish cupcake marzipan.</p>
+        <p> Topping fruitcake gummi bears fruitcake caramels marzipan. Donut donut sugar plum topping gummi bears. Marzipan soufflé sweet roll chocolate bar muffin chupa chups lemon drops cheesecake. Gummi bears donut croissant powder. Sesame snaps jelly beans caramels gummies macaroon liquorice. Cake biscuit powder oat cake danish candy canes. Jelly beans muffin marshmallow chupa chups gingerbread muffin cake jujubes jelly. Halvah jelly-o bear claw oat cake. Cookie pastry carrot cake biscuit gummies. Danish cupcake marzipan.</p>
+        </div>
+  
 </section>
---
+++ wp-content/themes/remember-cambridge/sections/text-and-image.php
@@ -0,0 +1,16 @@
+<section class="text-and-image ">
+    <div class="flex-grid container">
+        <div class="col-6 img">
+            
+
+        </div>
+        <div class="col-6 text">
+            text
+
+        </div>
+    </div>
+
+  
+    
+
+</section>
