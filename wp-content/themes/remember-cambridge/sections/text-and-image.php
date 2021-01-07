7c240ffb7fa392c05fc8c9749b99e2f72cda385c
leeoneill102030 <leeoneill102030@gmail.com>
2021-01-07 20:32:13 +0000

    image flex

--- wp-content/themes/remember-cambridge/sections/text-and-image.php
+++ wp-content/themes/remember-cambridge/sections/text-and-image.php
@@ -1,8 +1,9 @@
 <section class="text-and-image ">
     <div class="flex-grid container">
-        <div class="col-6 img">
-            
 
+        <div class="col-6 img flex-center-left">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/ph.jpg">
+            
         </div>
         <div class="col-6 text">
             text
