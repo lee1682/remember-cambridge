e5d34ffe302a65b3e3144b302d85b0a59d7a4ac2
lee1682 <lee@cubiq.co.uk>
2021-02-11 22:48:54 +0000

    text and image

--- wp-content/themes/remember-cambridge/template-pages/template-contents.php
+++ wp-content/themes/remember-cambridge/template-pages/template-contents.php
@@ -8,4 +8,5 @@
 
 <?php get_template_part("template-parts/intro-text");?>
 
+<!-- should become custom post -->
 <?php get_template_part("/template-parts/content-item");?>
--- wp-content/themes/remember-cambridge/template-parts/content-item.php
+++ wp-content/themes/remember-cambridge/template-parts/content-item.php
@@ -16,8 +16,7 @@
 </seciton>
 
 <seciton class="content-item">
-
-    <div class="container-large container-row">
+    <div class="container-large container-row reverse">
         <div class="content-item_text col-12 col-lg-6">
             <h2> Icecream </h2>
             <p> Candy canes cupcake tootsie roll fruitcake chocolate cake muffin wafer. Muffin gingerbread gingerbread
@@ -26,25 +25,6 @@
                 Biscuit caramels powder candy canes. Wafer wafer chocolate bar cake sesame snaps fruitcake halvah. Bear
                 claw marzipan candy chupa chups apple pie cake liquorice toffee cookie. </p>
         </div>
-
-        <div class="content-item_image col-12 col-lg-6">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ph.jpg">
-        </div>
-    </div>
-</seciton>
-
-<seciton class="content-item">
-
-    <div class="container-large container-row">
-        <div class="content-item_text col-12 col-lg-6">
-            <h2> Icecream </h2>
-            <p> Candy canes cupcake tootsie roll fruitcake chocolate cake muffin wafer. Muffin gingerbread gingerbread
-                chocolate cake cotton candy pie pie lemon drops. Bear claw caramels chocolate brownie. Soufflé tiramisu
-                wafer.
-                Biscuit caramels powder candy canes. Wafer wafer chocolate bar cake sesame snaps fruitcake halvah. Bear
-                claw marzipan candy chupa chups apple pie cake liquorice toffee cookie. </p>
-        </div>
-
         <div class="content-item_image col-12 col-lg-6">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ph.jpg">
         </div>
