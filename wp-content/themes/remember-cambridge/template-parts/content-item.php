d3fdaf11aee44fbe3396b8d4712be117bccd019d
lee1682 <lee@cubiq.co.uk>
2021-02-17 00:08:30 +0000

    content rows

--- wp-content/themes/remember-cambridge/template-parts/content-item.php
+++ wp-content/themes/remember-cambridge/template-parts/content-item.php
@@ -8,7 +8,6 @@
                 Biscuit caramels powder candy canes. Wafer wafer chocolate bar cake sesame snaps fruitcake halvah. Bear
                 claw marzipan candy chupa chups apple pie cake liquorice toffee cookie. </p>
         </div>
-
         <div class="content-item_image col-12 col-lg-6">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ph.jpg">
         </div>
@@ -30,3 +29,24 @@
         </div>
     </div>
 </seciton>
+
+
+<!-- 
+<?php 
+			if( have_rows('page_sections') ):
+				
+			    while ( have_rows('page_sections') ) : the_row();
+			
+			        if( get_row_layout() == 'subjects' ):
+			
+			        	get_template_part('sections/subjects');
+
+			        elseif( get_row_layout() == 'logos' ): 
+			
+			        	get_template_part('sections/logos');
+			        endif;
+			    endwhile;
+			else :
+			endif;
+			 
+			?> -->
